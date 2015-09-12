9db9e9b3c087e0e8ece85b7e2a9684e917f1b351
Michael P. Geraci <me@mgeraci.com>
2015-09-12 11:15:08 -0400

    add a json endpoint and return some date/time info

--- auto-hud.py
+++ auto-hud.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
+from flask import request
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -7,5 +10,15 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
+@app.route("/data")
+def data():
+    d = datetime.now()
+
+    data = {}
+    data["time"] = d.strftime("%H:%M")
+    data["date"] = d.strftime("%B %-d, %Y")
+
+    return jsonify(data)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- static/js/app.coffee
+++ static/js/app.coffee
@@ -1,2 +1,17 @@
 $ ->
-	console.log "pageload"
+	AutoHUD.init()
+
+window.AutoHUD = {
+	init: ->
+		@fetchData()
+
+	fetchData: ->
+		$.ajax("/data", {
+			type: "GET"
+			success: (data) =>
+				@parseData(data)
+		})
+
+	parseData: (data) ->
+		console.log data
+}
