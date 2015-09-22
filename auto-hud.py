99f150fc20df4f7786f98d32388c1c51a8b34a5c
Michael P. Geraci <me@mgeraci.com>
2015-09-21 21:36:11 -0400

    remove unused python module

--- auto-hud.py
+++ auto-hud.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask import render_template
 from flask import jsonify
-from flask import request
 from flask import Response
 from datetime import datetime
 import requests
